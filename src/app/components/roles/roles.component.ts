f8ec006b0c87f9570d13f54a899b1ea3c948af81
ashanwithana <ashanwithana4@gmail.com>
2025-03-13 10:21:24 +0530

    refactor roles component to master component and implement role fetching functionality

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css',
 })
-export class RolesComponent {
-  firstName: string = 'John';
-  lastName: string = 'Doe';
-  age: number = 30;
-  is_active: boolean = false;
-  currentDate: Date = new Date();
-  inputType: string = 'checkbox';
-  selectedState: string = '';
+export class RolesComponent implements OnInit {
+  // firstName: string = 'John';
+  // lastName: string = 'Doe';
+  // age: number = 30;
+  // is_active: boolean = false;
+  // currentDate: Date = new Date();
+  // inputType: string = 'checkbox';
+  // selectedState: string = '';
 
-  showWelcomeAlert() {
-    alert('Welcome to Angular 19');
+  // showWelcomeAlert() {
+  //   alert('Welcome to Angular 19');
+  // }
+  // showMessage(message: string) {
+  //   alert(message);
+  // }
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getAllRoles()
   }
-  showMessage(message: string) {
-    alert(message);
+
+  getAllRoles() {
+    this.http
+      .get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles')
+      .subscribe((res: any) => {
+        this.roleList = res.data;
+      });
   }
 }
